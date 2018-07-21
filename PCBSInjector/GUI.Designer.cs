b0ec4c2f8bb9be1a248876f75f5c86aca61be3ce
synogen <synogen@freenet.de>
2018-07-21 18:36:50 +0200

    added PCBS modloader injector again, simple working version

---
+++ PCBSInjector/GUI.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace PCBSInjector
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
+            this.InformationBox = new System.Windows.Forms.GroupBox();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.removeBtn = new System.Windows.Forms.Button();
+            this.installBtn = new System.Windows.Forms.Button();
+            this.gamePathBtn = new System.Windows.Forms.Button();
+            this.gamePathLabel = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.InformationBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InformationBox
+            // 
+            this.InformationBox.Controls.Add(this.pathLabel);
+            this.InformationBox.Controls.Add(this.removeBtn);
+            this.InformationBox.Controls.Add(this.installBtn);
+            this.InformationBox.Controls.Add(this.gamePathBtn);
+            this.InformationBox.Controls.Add(this.gamePathLabel);
+            this.InformationBox.Location = new System.Drawing.Point(13, 13);
+            this.InformationBox.Name = "InformationBox";
+            this.InformationBox.Size = new System.Drawing.Size(423, 81);
+            this.InformationBox.TabIndex = 0;
+            this.InformationBox.TabStop = false;
+            this.InformationBox.Text = "Information";
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(74, 20);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(0, 13);
+            this.pathLabel.TabIndex = 5;
+            // 
+            // removeBtn
+            // 
+            this.removeBtn.Enabled = false;
+            this.removeBtn.Location = new System.Drawing.Point(287, 48);
+            this.removeBtn.Name = "removeBtn";
+            this.removeBtn.Size = new System.Drawing.Size(130, 23);
+            this.removeBtn.TabIndex = 4;
+            this.removeBtn.Text = "Remove Modloader";
+            this.removeBtn.UseVisualStyleBackColor = true;
+            // 
+            // installBtn
+            // 
+            this.installBtn.Enabled = false;
+            this.installBtn.Location = new System.Drawing.Point(148, 48);
+            this.installBtn.Name = "installBtn";
+            this.installBtn.Size = new System.Drawing.Size(130, 23);
+            this.installBtn.TabIndex = 3;
+            this.installBtn.Text = "Install Modloader";
+            this.installBtn.UseVisualStyleBackColor = true;
+            this.installBtn.Click += new System.EventHandler(this.installBtn_Click);
+            // 
+            // gamePathBtn
+            // 
+            this.gamePathBtn.Location = new System.Drawing.Point(10, 48);
+            this.gamePathBtn.Name = "gamePathBtn";
+            this.gamePathBtn.Size = new System.Drawing.Size(130, 23);
+            this.gamePathBtn.TabIndex = 2;
+            this.gamePathBtn.Text = "Game Path";
+            this.gamePathBtn.UseVisualStyleBackColor = true;
+            this.gamePathBtn.Click += new System.EventHandler(this.gamePathBtn_Click);
+            // 
+            // gamePathLabel
+            // 
+            this.gamePathLabel.AutoSize = true;
+            this.gamePathLabel.Location = new System.Drawing.Point(7, 20);
+            this.gamePathLabel.Name = "gamePathLabel";
+            this.gamePathLabel.Size = new System.Drawing.Size(66, 13);
+            this.gamePathLabel.TabIndex = 0;
+            this.gamePathLabel.Text = "Game Path: ";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 100);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(423, 23);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 1;
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(12, 129);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(90, 13);
+            this.progressLabel.TabIndex = 6;
+            this.progressLabel.Text = "Select game path";
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 151);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.InformationBox);
+            this.Controls.Add(this.progressBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GUI";
+            this.Text = "Modloader Injector for PC Building Simulator";
+            this.InformationBox.ResumeLayout(false);
+            this.InformationBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox InformationBox;
+        private System.Windows.Forms.Button removeBtn;
+        private System.Windows.Forms.Button installBtn;
+        private System.Windows.Forms.Button gamePathBtn;
+        private System.Windows.Forms.Label gamePathLabel;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label progressLabel;
+    }
+}
+
---
+++ PCBSInjector/GUI.cs
@@ -0,0 +1,117 @@
+﻿using Mono.Cecil;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+using Mono.Cecil.Cil;
+
+namespace PCBSInjector
+{
+    public partial class GUI : Form
+    {
+        private string assemblySubPath = "/PCBS_Data/Managed";
+
+        public GUI()
+        {
+            InitializeComponent();
+            progressBar.ForeColor = Color.Green;
+        }
+
+        private void gamePathBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fileDialog = new FolderBrowserDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.pathLabel.Text = fileDialog.SelectedPath;
+                this.installBtn.Enabled = true;
+
+                try
+                {
+                    bool isInjected = IsInjected(pathLabel.Text + assemblySubPath, "Assembly-CSharp-firstpass.dll", "LogoSplash", "Awake", "PCBSModloader.dll", "PCBSModloader.ModLoader", "Init");
+                    if (isInjected)
+                    {
+                        StatusAlreadyInstalled();   
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+                }
+            }
+        }
+
+        private void StatusAlreadyInstalled()
+        {
+            this.installBtn.Enabled = false;
+            progressBar.Value = 100;
+            progressLabel.Text = "Modloader already installed!";
+        }
+
+        private void StatusInstalledSuccessfully()
+        {
+            this.installBtn.Enabled = false;
+            progressBar.Value = 100;
+            progressLabel.Text = "Modloader successfully installed!";
+        }
+
+        private void installBtn_Click(object sender, EventArgs e)
+        {
+            string mainPath = pathLabel.Text + assemblySubPath;
+            File.Copy(Directory.GetCurrentDirectory() + "/PCBSModloader.dll", mainPath + "/PCBSModloader.dll", true);
+            File.Copy(Directory.GetCurrentDirectory() + "/0Harmony.dll", mainPath + "/0Harmony.dll", true);
+            try
+            {
+                Inject(mainPath, "Assembly-CSharp-firstpass.dll", "LogoSplash", "Awake", "PCBSModloader.dll", "PCBSModloader.ModLoader", "Init");
+                StatusInstalledSuccessfully();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private void Inject(string mainPath, string assemblyToPatch, string assemblyType, string assemblyMethod, string loaderAssembly, string loaderType, string loaderMethod)
+        {
+            DefaultAssemblyResolver resolver = new DefaultAssemblyResolver();
+            resolver.AddSearchDirectory(mainPath);
+
+            using (ModuleDefinition
+                assembly = ModuleDefinition.ReadModule(mainPath + "/" + assemblyToPatch, new ReaderParameters { ReadWrite = true, AssemblyResolver = resolver }),
+                loader = ModuleDefinition.ReadModule(mainPath + "/" + loaderAssembly)
+            )
+            {
+                MethodDefinition methodToInject = loader.GetType(loaderType).Methods.Single(x => x.Name == loaderMethod);
+                MethodDefinition methodToHook = assembly.GetType(assemblyType).Methods.First(x => x.Name == assemblyMethod);
+
+                Instruction loaderInit = Instruction.Create(OpCodes.Call, assembly.ImportReference(methodToInject));
+                ILProcessor processor = methodToHook.Body.GetILProcessor();
+                processor.InsertBefore(methodToHook.Body.Instructions[0], loaderInit);
+
+                assembly.Write();
+            }
+        }
+
+        private bool IsInjected(string mainPath, string assemblyToPatch, string assemblyType, string assemblyMethod, string loaderAssembly, string loaderType, string loaderMethod)
+        {
+            using (ModuleDefinition
+                assembly = ModuleDefinition.ReadModule(mainPath + "/" + assemblyToPatch),
+                loader = ModuleDefinition.ReadModule(mainPath + "/" + loaderAssembly)
+            )
+            {
+                MethodDefinition methodToInject = loader.GetType(loaderType).Methods.Single(x => x.Name == loaderMethod);
+                MethodDefinition methodToHook = assembly.GetType(assemblyType).Methods.First(x => x.Name == assemblyMethod);
+
+                foreach (Instruction instruction in methodToHook.Body.Instructions)
+                {
+                    if (instruction.OpCode.Equals(OpCodes.Call) && instruction.Operand.ToString().Equals($"System.Void {loaderType}::{loaderMethod}()"))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+           
+        }
+    }
+}
