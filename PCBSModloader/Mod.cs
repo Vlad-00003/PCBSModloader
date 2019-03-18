d70de928438ce166a6ab7ab5f4d3086cae2b7e5e
Vlad-00003 <Vlad-00003@mail.ru>
2019-03-18 16:05:23 +0300

    Docs

--- PCBSModloader/Mod.cs
+++ PCBSModloader/Mod.cs
@@ -1,23 +1,68 @@
 ﻿namespace PCBSModloader
 {
+    /// <summary>
+    /// Basic class that every mod should extend
+    /// </summary>
     public abstract class Mod
     {
+        /// <summary>
+        /// Indicates if the mod has any texture files that must be loaded from "PCBS/mods/ID/Textures"
+        /// </summary>
         public virtual bool HasTextures => false;
+        /// <summary>
+        /// Indicates if the mod has any AssetBundles that must be loaded from "PCBS/mods/ID/Textures"
+        /// </summary>
         public virtual bool HasAssetBundles => false;
 
+        /// <summary>
+        /// ID of the mod. Same as folder.
+        /// </summary>
         public abstract string ID { get; }
+        /// <summary>
+        /// User-friendly name of the mod.
+        /// </summary>
         public virtual string Name { get { return ID; } }
+        /// <summary>
+        /// Current version of the mod.
+        /// </summary>
         public abstract string Version { get; }
+        /// <summary>
+        /// Author of the mod.
+        /// </summary>
         public abstract string Author { get; }
 
+        /// <summary>
+        /// Hook that would be called as soon as the mod has been loaded.
+        /// </summary>
         public virtual void OnInit() { }
+        /// <summary>
+        /// Hook that would be called as soon as the game would be started (Continue\New game).
+        /// https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager-sceneLoaded.html
+        /// </summary>
         public virtual void OnGame() { }
+        /// <summary>
+        /// https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnGUI.html
+        /// </summary>
         public virtual void OnGUI() { }
+        /// <summary>
+        /// MonoBehaviour.Update()
+        /// https://docs.unity3d.com/ScriptReference/MonoBehaviour.Update.html
+        /// </summary>
         public virtual void Update() { }
+        /// <summary>
+        /// MonoBehaviour.FixedUpdate()
+        /// https://docs.unity3d.com/ScriptReference/MonoBehaviour.FixedUpdate.html
+        /// </summary>
         public virtual void FixedUpdate() { }
 
+        /// <summary>
+        /// TODO: NOT IMPLEMENTED YET
+        /// </summary>
         public virtual void Stop() { }
 
+        /// <summary>
+        /// Working directory of the mod.
+        /// </summary>
         public virtual string Modpath { get { return ModLoader.ModsPath + "/" + ID; } }
     }
 }
--- PCBSModloader/ModLogs.cs
+++ PCBSModloader/ModLogs.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 
 namespace PCBSModloader
 {
+    /// <summary>
+    /// Provides an easy way to log information
+    /// </summary>
     public class ModLogs : MonoBehaviour
     {
+        /// <summary>
+        /// Path to the logfile.
+        /// </summary>
         public static readonly string LogsPath = GetGameRootPath() + "/Mods/log.txt";
 
+        /// <summary>
+        /// Removes the existing logfile.
+        /// </summary>
         public static void EnableLogs()
         {
             if (File.Exists(LogsPath))
@@ -17,6 +26,10 @@ namespace PCBSModloader
             }
         }
 
+        /// <summary>
+        /// Append text to the logfile.
+        /// </summary>
+        /// <param name="logString"></param>
         public static void Log(string logString)
         {
             File.AppendAllText(LogsPath, logString + Environment.NewLine);
